85caf496c66f1dbf5b449bace38bba686677e596
olson3r <scootles.olson@gmail.com>
2018-02-28 07:38:02 -0600

    Add working async pin poller code to main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
-#[cfg(feature = "tokio")]
-extern crate futures;
-#[cfg(feature = "tokio")]
+#[cfg(feature = "mio-evented")]
+extern crate mio;
+#[cfg(feature = "mio-evented")]
 extern crate sysfs_gpio;
-#[cfg(feature = "tokio")]
-extern crate tokio_core;
 
+#[cfg(feature = "mio-evented")]
+use mio::{Events, Poll, PollOpt, Ready, Token};
+#[cfg(feature = "mio-evented")]
+use sysfs_gpio::{AsyncPinPoller, Direction, Edge, Pin};
 use std::thread;
-#[cfg(feature = "tokio")]
-use futures::{Future, Stream, Sink};
-#[cfg(feature = "tokio")]
-use sysfs_gpio::{Direction, Edge, Pin};
-#[cfg(feature = "tokio")]
-use tokio_core::reactor::Core;
-#[cfg(feature = "tokio")]
-use futures::sync::mpsc;
+use std::time::Duration;
 
 #[derive(Debug)]
 enum StreamError {
@@ -34,67 +29,46 @@ impl From<sysfs_gpio::Error> for StreamError {
   }
 }
 
-#[cfg(feature = "tokio")]
+const TOKEN_0: Token = Token(0);
+const TOKEN_1: Token = Token(1);
+
+#[cfg(feature = "mio-evented")]
 fn stream() -> StreamResult {
-  let pin_nums = [(17, 0), (27, 1)];
+  let pin_nums = [(17, TOKEN_0), (27, TOKEN_1)];
+  // let pin_nums = [(17, TOKEN_0)];
   let pins: Vec<_> = pin_nums.iter()
-    .map(|&(pin_num, value)| (value, Pin::new(pin_num)))
+    .map(|&(pin_num, token)| (token, Pin::new(pin_num)))
     .collect();
-  let mut core = Core::new()?;
-  let handle = core.handle();
-  let (tx_master, mut rx) = mpsc::channel(50);
-  for &(value, ref pin) in pins.iter() {
-    println!("Spawning {}", pin.get_pin_num());
-    let tx = tx_master.clone();
-    pin.export()?;
-    pin.set_direction(Direction::In)?;
-    pin.set_edge(Edge::FallingEdge)?;
-    handle.spawn(pin.get_stream(&handle)?
-      .for_each(move |_| {
-        println!("PPP{}", value);
-        tx.clone()
-          .send(value)
-          .then(|tx|
-            match tx {
-              Ok(_tx) => {
-                println!("Sink flushed!");
-                Ok(())
-              }
-              Err(err) => {
-                println!("Error: {:?}", err);
-                Err(err)
-              }
-            }
-          );
-        Ok(())
-      })
-      .map_err(|_| ())
-    );
-  }
 
-  // thread::spawn(move || {
-    loop {
-      println!("Loop");
-      core.turn(Some(std::time::Duration::from_millis(1000)));
-      // rx.for_each(|value| {
-      //     println!("VAL{}", value);
-      //   });
-      match rx.poll() {
-        Ok(value) => {
-          println!("P{:?}", value);
-          ()
+  let poll = Poll::new()?;
+  let async_pin_pollers: Vec<AsyncPinPoller> = pins.iter()
+    .map(|&(token, ref pin)| {
+      println!("Setting up gpio{}", pin.get_pin_num());
+      pin.export().unwrap();
+      thread::sleep(Duration::from_millis(100)); // Delay for sysfs to export the files
+      pin.set_direction(Direction::In).unwrap();
+      pin.set_edge(Edge::FallingEdge).unwrap();
+
+      let async_pin_poller = pin.get_async_poller().unwrap();
+      poll.register(&async_pin_poller, token, Ready::readable(), PollOpt::edge()).unwrap();
+      async_pin_poller // Need to keep a valid reference to the async poller
+    })
+    .collect();
+
+  let mut events = Events::with_capacity(1024);
+  loop {
+    poll.poll(&mut events, Some(Duration::from_millis(1000)))?;
+    for event in &events {
+      match event.token() {
+        Token(i) => {
+          // data.push(i as u8);
         }
-        Err(_) => ()
       }
-      // Ok(())
     }
-  // }
-
-  // core.run(r2).unwrap();
-  // Ok(())
+  }
 }
 
-#[cfg(feature = "tokio")]
+#[cfg(feature = "mio-evented")]
 fn main() {
   match stream() {
     Ok(()) => println!("Stream complete"),
@@ -102,7 +76,7 @@ fn main() {
   }
 }
 
-#[cfg(not(feature = "tokio"))]
+#[cfg(not(feature = "mio-evented"))]
 fn main() {
-  println!("This example requires the `tokio` feature to be enabled.");
+  println!("This example requires the `mio-evented` feature to be enabled.");
 }
