8605e76a14d15af6baab56d400decd5f0b696106
olson3r <scootles.olson@gmail.com>
2018-02-28 09:12:16 -0600

    Split up into a wait loop and read loop

--- examples/data_buffer.rs
+++ examples/data_buffer.rs
@@ -57,50 +57,76 @@ impl<'a> RfidReader<'a> {
   pub fn new(rx: Receiver<u8>) -> RfidReader<'a> {
     let bit_buffer =  Vec::new();
     let pin_key_buffer = Vec::new();
+    let pin_key_buffer_last_add_dt =  Instant::now();
     let key_mapper = KeyMapper::new();
     RfidReader {
       rx: rx,
       bit_buffer: bit_buffer,
       pin_key_buffer: pin_key_buffer,
+      pin_key_buffer_last_add_dt: pin_key_buffer_last_add_dt,
       key_mapper: key_mapper
     }
   }
 
   pub fn start(&mut self) {
     loop {
-      read_data();
+      self.wait_for_data();
     }
   }
 
-  fn read_data(&self) {
-    match self.rx.recv_timeout(Duration::from_millis(1000)) {
-      Ok(bit) => self.add_bit(bit),
+  fn wait_for_data(&mut self) {
+    let timeout = Duration::from_millis(RFID_WAIT_TIMEOUT_MS);
+    match self.rx.recv_timeout(timeout) {
+      Ok(bit) => {
+        self.add_bit(bit);
+        self.read_data();
+      },
       Err(RecvTimeoutError::Timeout) => {
-        let value: String =
-        println!("Buffer Timeout: trying to match {}", self.bits());
-        match self.key_mapper.key(&self.bits()) {
-          Some("#") => {
-            println!("AUTHORIZE PIN: {:?}", self.pin_key_buffer);
-            self.clear_bit_buffer();
-          },
-          Some("*") => {
-            println!("CLEAR");
-            self.clear_bit_buffer();
-          },
-          Some(digit) => {
-            self.clear_bit_buffer();
-            self.pin_key_buffer.push(digit);
-            println!("DATA {:?}", self.pin_key_buffer);
-          },
-          None => {
-            println!("AUTHORIZE RFID: {:?}", self.bit_buffer);
-            self.clear_bit_buffer();
-          }
+        println!("TIMEOUT: BITS {:?}, PIN KEYS: {:?}", self.bits(), self.pin_keys());
+        if self.pin_key_buffer_is_stale() {
+          self.clear_pin_key_buffer()
         }
       },
       Err(err) =>  println!("Buffer Error: {:?}", err)
     }
-    println!("BIT BUFFER {:?}", self.bit_buffer);
+  }
+
+  fn read_data(&mut self) {
+    let timeout = Duration::from_millis(RFID_READ_TIMEOUT_MS);
+    loop {
+      match self.rx.recv_timeout(timeout) {
+        Ok(bit) => self.add_bit(bit),
+        Err(RecvTimeoutError::Timeout) => {
+          println!("Buffer Timeout: trying to match {}", self.bits());
+          match self.key_mapper.key(&self.bits()) {
+            Some("#") => {
+              println!("AUTHORIZE PIN: {:?}", self.pin_keys());
+              self.clear_bit_buffer();
+              self.clear_pin_key_buffer();
+            },
+            Some("*") => {
+              self.clear_bit_buffer();
+            },
+            Some(pin_key) => {
+              self.clear_bit_buffer();
+              self.add_pin_key(pin_key);
+              println!("PIN {:?}", self.pin_key_buffer);
+            },
+            None => {
+              println!("AUTHORIZE RFID: {:?}", self.bits());
+              self.clear_bit_buffer();
+              self.clear_pin_key_buffer();
+            }
+          }
+          break;
+        },
+        Err(err) => {
+          println!("Buffer Error: {:?}", err);
+          break;
+        }
+      }
+      println!("BITS {:?}, PIN KEYS: {:?}", self.bits(), self.pin_keys());
+    }
   }
 
   fn add_bit(&mut self, bit: u8) {
@@ -110,15 +136,32 @@ impl<'a> RfidReader<'a> {
   fn bits(&self) -> String {
     self.bit_buffer.iter()
       .map(|bit| bit.to_string())
-      .collect();
+      .collect::<String>()
   }
 
-  fn add_pin_key(&mut self, pin_key: &str) {
-    self.pin_key_buffer.push(pin_key)
+  fn pin_key_buffer_is_stale(&self) -> bool {
+    if self.pin_key_buffer.is_empty() { return false }
+    let timeout = Duration::from_secs(PIN_KEY_BUFFER_STALE_TIMEOUT_SEC);
+    self.pin_key_buffer_last_add_dt.elapsed() > timeout
   }
 
   fn clear_bit_buffer(&mut self) {
+    println!("CLEAR BIT BUFFER");
     self.bit_buffer.clear();
+  }
+
+  fn add_pin_key(&mut self, pin_key: &'a str) {
+    self.pin_key_buffer.push(pin_key);
+    self.pin_key_buffer_last_add_dt = Instant::now();
+  }
+
+  fn pin_keys(&self) -> String {
+    self.pin_key_buffer.concat()
+  }
+
+  fn clear_pin_key_buffer(&mut self) {
+    println!("CLEAR PIN KEY BUFFER");
+    self.pin_key_buffer.clear();
   }
 }
 
