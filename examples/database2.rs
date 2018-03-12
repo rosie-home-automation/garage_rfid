21ea2eac2a7694721beb5f6df33436c713d10915
olson3r <scootles.olson@gmail.com>
2018-03-12 08:11:38 -0500

    Add credentials query

--- examples/database2.rs
+++ examples/database2.rs
@@ -9,6 +9,7 @@ use std::env;
 
 use garage_rfid::models::user::User;
 use garage_rfid::models::credential::Credential;
+use garage_rfid::schema::users;
 
 fn establish_connection() -> SqliteConnection {
   dotenv().ok();
@@ -26,13 +27,21 @@ fn create_credential(connection: &SqliteConnection, user: &User) -> Credential {
   let name = "Taco RFID".to_string();
   let variety = "RFID".to_string();
   let value = "101100101100".to_string();
-  Credential::create(connection, user.id, name, variety, value)
+  Credential::create(connection, &user.id, name, variety, value)
 }
 
 fn test1() {
   let connection = establish_connection();
-  let user = creat_user(&connection, "Matilda".to_string());
-  println!("CREATED USER {:?}", user);
+  // let user = creat_user(&connection, "Matilda".to_string());
+  // let credential = create_credential(&connection, &user);
+  // println!("CREATED USER {:?}", user);
+
+  let user_id = "dea3f799-f6da-423d-aecf-21518a8b76ad";
+  let user = users::table.find(user_id).first::<User>(&connection)
+    .expect(&format!("Error finding user for id {}", user_id));
+  let credentials = Credential::belonging_to(&user).load::<Credential>(&connection)
+    .expect("Error loading credentials");
+  println!("Credentials {:?}", credentials);
 }
 
 fn main() {
