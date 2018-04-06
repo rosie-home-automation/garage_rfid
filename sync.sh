d159d5e8c463816d983f3378779a609840cc3def
olson3r <scootles.olson@gmail.com>
2018-04-06 09:45:21 -0500

    We want the cargo lock

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 rsync -arvuzc garage_pi.local:/home/pi/garage_rfid/migrations/ ./migrations/
 rsync -arvuzc garage_pi.local:/home/pi/garage_rfid/src/schema.rs ./src/schema.rs
+rsync -arvuzc garage_pi.local:/home/pi/garage_rfid/Cargo.lock ./Cargo.lock
 rsync -arvuzc --delete garage_pi.local:/home/pi/garage_rfid/logs/ ./logs/
-rsync -arvzc --delete --exclude garage_rfid.sqlite3 --exclude target/ ./ garage_pi.local:/home/pi/garage_rfid/
+rsync -arvzc --delete --exclude garage_rfid.sqlite3 --exclude target/  --exclude .git/ ./ garage_pi.local:/home/pi/garage_rfid/
