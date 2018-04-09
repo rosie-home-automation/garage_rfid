fe31ee9578381faa19a0893c2705e1b981a8ede6
olson3r <scootles.olson@gmail.com>
2018-04-08 19:58:20 -0500

    Add users#show endpoint

--- src/users_controller.rs
+++ src/users_controller.rs
@@ -1,6 +1,7 @@
+use diesel;
 use diesel::prelude::*;
 use gotham::http::response::create_response;
-use gotham::state::State;
+use gotham::state::{FromState, State};
 use hyper::{Response, StatusCode};
 use mime;
 use serde_json;
@@ -10,6 +11,12 @@ use database::Database;
 use models::user::User;
 use schema::users::dsl::*;
 
+#[derive(Deserialize, StateData, StaticResponseExtender)]
+pub struct UserPathParams {
+  pub id: String
+}
+
+#[derive(Clone)]
 pub struct UsersController {
   database: Database,
   logger: slog::Logger,
@@ -57,6 +64,57 @@ impl UsersController {
           Some((vec![], mime::APPLICATION_JSON)),
         );
         (state, response)
+      }
+    }
+  }
+
+  pub fn show(&self, mut state: State) -> (State, Response) {
+    let connection = self.connection();
+    let UserPathParams { id: user_id } = UserPathParams::take_from(&mut state);
+    let user_response = users.find(&user_id).first::<User>(&connection);
+    match user_response {
+      Ok(user) => {
+        let user_json_result = serde_json::to_string(&user);
+        match user_json_result {
+          Ok(user_json) => {
+            let response = create_response(
+              &state,
+              StatusCode::Ok,
+              Some((user_json.into_bytes(), mime::APPLICATION_JSON)),
+            );
+            (state, response)
+          },
+          Err(err) => {
+            error!(self.logger, "Error converting user to json."; "err" => ?err); // TODO: Add trace id
+            let response = create_response(
+              &state,
+              StatusCode::InternalServerError,
+              Some((vec![], mime::APPLICATION_JSON)),
+            );
+            (state, response)
+          }
+        }
+      },
+      Err(err) => {
+        error!(self.logger, "Error loading user."; "err" => ?err, "id" => &user_id); // TODO: Add trace id
+        match err {
+          diesel::result::Error::NotFound => {
+            let response = create_response(
+              &state,
+              StatusCode::NotFound,
+              Some((vec![], mime::APPLICATION_JSON)),
+            );
+            (state, response)
+          },
+          _ => {
+            let response = create_response(
+              &state,
+              StatusCode::InternalServerError,
+              Some((vec![], mime::APPLICATION_JSON)),
+            );
+            (state, response)
+          }
+        }
       }
     }
   }
