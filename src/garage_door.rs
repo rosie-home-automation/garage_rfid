3196567d0d2c08d15733916fca650774b509c85d
olson3r <scootles.olson@gmail.com>
2018-06-08 13:36:59 -0500

    Got the garage door working

--- src/garage_door.rs
+++ src/garage_door.rs
@@ -1,7 +1,9 @@
 use futures::{Future, Stream};
 use slog;
-use std::sync::mpsc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use std::thread;
+use std::time::Duration;
 use sysfs_gpio;
 use tokio_core::reactor::Core;
 
@@ -15,8 +17,11 @@ pub struct GarageDoor {
   open_led_gpio: usize,
   closed_led_gpio: usize,
   async_pin_pollers: Vec<sysfs_gpio::AsyncPinPoller>,
+  opener: sysfs_gpio::Pin,
+  open_led: Arc<sysfs_gpio::Pin>,
+  closed_led: Arc<sysfs_gpio::Pin>,
   logger: slog::Logger,
-  tmp_status: bool,
+  is_open: Arc<AtomicBool>,
 }
 
 impl GarageDoor {
@@ -25,67 +30,113 @@ impl GarageDoor {
     let opener_gpio = configuration.garage_door.opener_gpio;
     let open_led_gpio = configuration.garage_door.open_led_gpio;
     let closed_led_gpio = configuration.garage_door.closed_led_gpio;
+    let opener = GpioUtil::setup_output_pin(opener_gpio);
+    let open_led = GpioUtil::setup_output_pin(open_led_gpio);
+    let open_led = Arc::new(open_led);
+    let closed_led = GpioUtil::setup_output_pin(closed_led_gpio);
+    let closed_led = Arc::new(closed_led);
     let async_pin_pollers = Vec::new();
+    let is_open = Arc::new(AtomicBool::new(true));
     let mut garage_door = GarageDoor {
       sensor_gpio: sensor_gpio,
       opener_gpio: opener_gpio,
       open_led_gpio: open_led_gpio,
       closed_led_gpio: closed_led_gpio,
+      opener: opener,
+      open_led: open_led,
+      closed_led: closed_led,
       async_pin_pollers: async_pin_pollers,
       logger: logger,
-      tmp_status: false,
+      is_open: is_open,
     };
     garage_door.setup_logger();
     garage_door
   }
 
   pub fn start(&self) {
-    // let sensor_gpio = self.sensor_gpio;
-    // let logger = self.logger.clone();
-    // let _executor_thread = thread::spawn(move || {
-    //   let mut l = Core::new().expect("New tokio core.");
-    //   let handle = l.handle();
-    //   info!(logger, "Setting up sensor gpio"; "sensor_gpio" => sensor_gpio);
-    //   let sensor_pin = GpioUtil::setup_input_pin(sensor_gpio, sysfs_gpio::Edge::BothEdges);
-    //   handle.spawn(sensor_pin.get_value_stream(&handle)
-    //     .expect("Expected to get sensor pin value stream.")
-    //     .for_each(move |val| {
-    //       println!("Sensor pin changed value to {}", val);
-    //       Ok(())
-    //     })
-    //     .map_err(|_| ())
-    //   );
+    self.setup_sensor();
+  }
 
-    //   loop {
-    //     l.turn(None)
-    //   }
-    // });
+  fn setup_sensor(&self) {
+    let sensor_gpio = self.sensor_gpio;
+    let logger = self.logger.clone();
+    let is_open = self.is_open.clone();
+    let open_led = self.open_led.clone();
+    let closed_led = self.closed_led.clone();
+    let _executor_thread = thread::spawn(move || {
+      let mut l = Core::new().expect("New tokio core.");
+      let handle = l.handle();
+      let is_open = is_open.clone();
+      info!(logger, "Setting up sensor gpio"; "sensor_gpio" => sensor_gpio);
+      let sensor_pin = GpioUtil::setup_input_pin(sensor_gpio, sysfs_gpio::Edge::BothEdges);
+      handle.spawn(sensor_pin.get_value_stream(&handle)
+        .expect("Expected to get sensor pin value stream.")
+        .for_each(move |raw_sensor_value| {
+          let sensor_value = raw_sensor_value == 1;
+          println!("Sensor pin changed value to {}", sensor_value);
+          let is_open = is_open.clone();
+          let is_open_value = is_open.load(Ordering::Relaxed);
+          if is_open_value != sensor_value {
+            is_open.store(sensor_value, Ordering::Relaxed);
+            match sensor_value {
+              true => {
+                open_led.set_value(0).unwrap();
+                closed_led.set_value(1).unwrap();
+              },
+              false => {
+                open_led.set_value(1).unwrap();
+                closed_led.set_value(0).unwrap();
+              }
+            }
+          }
+          Ok(())
+        })
+        .map_err(|_| ())
+      );
+
+      loop {
+        l.turn(None)
+      }
+    });
   }
 
   pub fn status(&self) -> String {
-    match self.tmp_status {
+    match self.is_open() {
       true => "open".to_string(),
       false => "closed".to_string(),
     }
   }
 
-  pub fn open(&mut self) {
-    self.tmp_status = true;
+  pub fn open(&self) {
+    info!(self.logger, "Open triggered.");
+    if !self.is_open() {
+      self.trigger_opener();
+    }
   }
 
-  pub fn close(&mut self) {
-    self.tmp_status = false;
+  pub fn close(&self) {
+    info!(self.logger, "Close triggered.");
+    if self.is_open() {
+      self.trigger_opener();
+    }
   }
 
-  pub fn toggle(&mut self) {
-    self.tmp_status = !self.tmp_status;
+  pub fn toggle(&self) {
+    info!(self.logger, "Toggle triggered.");
+    self.trigger_opener();
+  }
+
+  fn trigger_opener(&self) {
+    self.opener.set_value(1).unwrap();
+    thread::sleep(Duration::from_millis(100));
+    self.opener.set_value(0).unwrap();
   }
 
   fn setup_logger(&mut self) {
     self.logger = self.logger.new(o!("garage_door" => format!("{:?}", self)));
   }
 
-  fn is_open(&self) {
-    self.tmp_status;
+  fn is_open(&self) -> bool {
+    self.is_open.load(Ordering::Relaxed)
   }
 }
--- src/gpio_util.rs
+++ src/gpio_util.rs
@@ -8,10 +8,19 @@ impl GpioUtil {
   pub fn setup_input_pin(pin_num: usize, edge: sysfs_gpio::Edge) -> sysfs_gpio::Pin {
     let pin = sysfs_gpio::Pin::new(pin_num as u64);
     pin.export().expect("Expected to export input pin.");
-    thread::sleep(Duration::from_millis(100)); // Delay for sysfs to export the files or you may get permission denied
+    thread::sleep(Duration::from_millis(120)); // Delay for sysfs to export the files or you may get permission denied
     pin.set_direction(sysfs_gpio::Direction::In)
       .expect("Expected to set pin direction to an input.");
     pin.set_edge(edge).expect("Expected to set input pin edge.");
+    pin
+  }
+
+  pub fn setup_output_pin(pin_num: usize) -> sysfs_gpio::Pin {
+    let pin = sysfs_gpio::Pin::new(pin_num as u64);
+    pin.export().expect("Expected to export input pin.");
+    thread::sleep(Duration::from_millis(120)); // Delay for sysfs to export the files or you may get permission denied
+    pin.set_direction(sysfs_gpio::Direction::Low)
+      .expect("Expected to set pin direction to an output.");
     pin
   }
 }
